853599e3b330708499cd729dbf9445c43502e959
RaffaellaBrugnini <raffaella.brugnini@gmail.com>
2025-04-22 14:50:33 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -70,7 +70,7 @@ function App() {
 
   // --- Caricamento CSV ---
   useEffect(() => {
-    const csvUrl = 'https://docs.google.com/spreadsheets/d/e/18avHrTcAt4oF6D1aQGskepGRThTFU13Wtg9SPS6epnk/edit?gid=544521169#gid=544521169';
+    const csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTcR6bZ3XeX-6tzjcoWpCws6k0QeJNdkaYJ8Q_IaJNkXUP3kWF75gSC51BK6hcJfloRWtMxD239ZCSq/pubhtml';
     Papa.parse(csvUrl, {
       download: true,
       header: true,
