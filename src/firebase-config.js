b83b1814f4fc3eb47b30d82473bb27bdfd511994
RaffaellaBrugnini <raffaella.brugnini@gmail.com>
2025-04-29 14:17:52 +0200

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,8 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAv4t6YPgvCm1kyaGxgw2aAL9NhRtM9Tcc",
@@ -15,3 +17,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
